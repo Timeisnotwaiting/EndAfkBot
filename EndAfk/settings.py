e59eba2f9d25ae41c9eedd8a80a042bf09757cda
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-25 19:17:28 +0530

    Update settings.py

--- EndAfk/settings.py
+++ EndAfk/settings.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB
 from .det import det
+from config import OWNER_USERNAME
+
 
 SETTINGS1 = [
             [IKB(" 🤵 Owner", url=f"t.me/{OWNER_USERNAME[1:]}"),
@@ -15,3 +17,24 @@ async def settings(_, m):
     await m.reply_photo("https://te.legra.ph/file/7637e88a7367abb6336d5.jpg", caption=f"""Hello! My name is {name}
 
 To know more about me check help section.""", reply_markup=IKM(SETTINGS1))
+
+TEXT_2 = f"""⚙️ AFK Bot Settings
+
+🖇Group: {m.chat.title}
+🔖Group ID: {m.chat.id}
+
+💡Choose the function buttons from below which you want to edit or change."""
+
+SETTINGS2_E = [
+            [
+             IKB("🔁 Clean Mode", data="CM"),
+             IKB("✅ Enabled", data="toggle_disbale"),
+            ]
+            ]
+
+SETTINGS2_D = [
+            [
+             IKB("🔁 Clean Mode", data="CM"),
+             IKB("❌ Disabled", data="toggle_enable"),
+            ]
+            ]
