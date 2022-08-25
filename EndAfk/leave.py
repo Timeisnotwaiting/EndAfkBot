67a214d826cf2557fa6a88a3bb05f8e715f008eb
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-25 12:21:33 +0530

    Update leave.py

--- EndAfk/leave.py
+++ EndAfk/leave.py
@@ -1,7 +1,9 @@
 from pyrogram import Client as Alpha, filters
 from pyrogram.types import Message
 from EndAfk import SUDOERS
-from .det import BOT_DET
+from .det import det
+
+BOT_DET = await det(Alpha)
 
 @Alpha.on_message(filters.command(f"leave@{BOT_DET[0]}") & filters.user(SUDOERS))
 async def leave(_, m):
