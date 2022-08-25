a3c4b534a3b134d51fd92282b3b3a87284dfca69
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-25 23:15:26 +0530

    Update help.py

--- EndAfk/help.py
+++ EndAfk/help.py
@@ -4,18 +4,18 @@ from .det import det
 async def help(_, m):
     d = await det(_)
     name = (await _.get_users(d[1]).first_name
-    await m.reply(f"""Welcome to {name}'s Help Section.
-
-- When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.
+    await m.reply(f"""Welcome to {name}'s Help Section.\n\n- When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.\n\n/afk - This will set you offline.\n\n/afk [Reason] - This will set you offline with a reason.\n\n/afk [Replied to a Sticker/Photo] - This will set you offline with an image or sticker.\n\n/afk [Replied to a Sticker/Photo] [Reason] - This will set you afk with an image and reason both.\n\n/settings - To change or edit basic settings of AFK Bot.""")
 
 
-/afk - This will set you offline.
-
-/afk [Reason] - This will set you offline with a reason.
-
-/afk [Replied to a Sticker/Photo] - This will set you offline with an image or sticker.
-
-/afk [Replied to a Sticker/Photo] [Reason] - This will set you afk with an image and reason both.
 
 
-/settings - To change or edit basic settings of AFK Bot.""")
+
+
+
+
+
+
+
+
+
+
