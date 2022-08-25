e0a8cac1d2e262fa4172e7832a69fd5e7abedfcc
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-25 18:48:56 +0530

    Update settings.py

--- EndAfk/settings.py
+++ EndAfk/settings.py
@@ -5,4 +5,7 @@ from .det import det
 @Client.on_message(filters.command("settings") & filters.group)
 async def settings(_, m):
     d = await det(_)
-    
+    name = (await _.get_users(d[1])).first_name
+    await m.reply(f"""Hello! My name is {name}
+
+To know more about me check help section.""")
