437b0a85116f9f05bcbb4e33f51669f727dcdae1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-25 18:51:53 +0530

    Update settings.py

--- EndAfk/settings.py
+++ EndAfk/settings.py
@@ -6,6 +6,6 @@ from .det import det
 async def settings(_, m):
     d = await det(_)
     name = (await _.get_users(d[1])).first_name
-    await m.reply(f"""Hello! My name is {name}
+    await m.reply_photo("https://te.legra.ph/file/7637e88a7367abb6336d5.jpg", caption=f"""Hello! My name is {name}
 
 To know more about me check help section.""")
