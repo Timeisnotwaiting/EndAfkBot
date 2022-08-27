782fde980347cd4c4f5fecac1eed130b40ea69ad
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-27 16:47:17 +0530

    Update det.py

--- EndAfk/det.py
+++ EndAfk/det.py
@@ -5,8 +5,10 @@ async def det(_):
     Le = await _.get_me()
     UN = Le["username"]
     id = Le["id"]
+    FN = Le["first_name"]
     BOT_DET = []
     BOT_DET.append(UN)
     BOT_DET.append(id)
+    BOT_DET.append(FN)
     return BOT_DET
     
