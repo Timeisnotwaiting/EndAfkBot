1e3039a2413c2d0f6c29d0dc18678c98e53c70eb
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-27 16:27:24 +0530

    Update __init__.py

--- EndAfk/__init__.py
+++ EndAfk/__init__.py
@@ -1,7 +1,9 @@
 import asyncio
 import time
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-
+from .det import det
+from pyrogram import Client
+from pyrogram.types import Message
 import config
 
 loop = asyncio.get_event_loop()
@@ -10,9 +12,11 @@ boot = time.time()
 mongo = MongoClient(config.MONGO_DB_URI)
 db = mongo.AFK
 
-botname = None
+get_ent = asyncio.run(det(Client, Message))
 
-botid = None
+botname = get_ent[0]
+
+botid = get_ent[1]
 
 
 SUDOERS = config.SUDO_USER
