a7f11e2c264d0d2aaad2c294322a67642de7a8bc
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-28 13:59:54 +0530

    Update brb.py

--- EndAfk/brb.py
+++ EndAfk/brb.py
@@ -1,1 +1,191 @@
+import time
+import random
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from EndAfk import SUDOERS
+from .helpers import RANDOM
+from .det import det
+from EndAfk.AlphaDB import add_afk, is_afk, remove_afk
+from EndAfk.helpers import get_readable_time, put_cleanmode
+from EndAfk.AlphaDB import is_blocked
 
+botusername = None
+
+@Client.on_message(filters.command("brb", ""))
+async def active_afk(_, message: Message):
+    global botusername
+    blocked = await is_blocked(message.from_user.id)
+    if blocked:
+        return
+    if message.sender_chat:
+        return
+    user_id = message.from_user.id
+    try:
+        await message.delete()
+    except:
+        pass
+    smex = await det(_)
+    botname = smex[2]
+    botusername = smex[0]
+    send = await message.reply_photo(random.choice(RANDOM),
+       caption=f"{message.from_user.first_name} is now away from keyboard ...!"
+    )
+    await put_cleanmode(message.chat.id, send.message_id)
+
+    verifier, reasondb = await is_afk(user_id)
+    if verifier:
+        await remove_afk(user_id)
+        try:
+            afktype = reasondb["type"]
+            timeafk = reasondb["time"]
+            data = reasondb["data"]
+            reasonafk = reasondb["reason"]
+            seenago = get_readable_time((int(time.time() - timeafk)))
+            if afktype == "text":
+                return await message.reply_text(
+                    f"**{message.from_user.first_name}** is back online and was away for {seenago}",
+                    disable_web_page_preview=True,
+                )
+            if afktype == "text_reason":
+                return await message.reply_text(
+                    f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}`",
+                    disable_web_page_preview=True,
+                )
+            if afktype == "animation":
+                if str(reasonafk) == "None":
+                    return await message.reply_animation(
+                        data,
+                        caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}",
+                    )
+                else:
+                    return await message.reply_animation(
+                        data,
+                        caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}",
+                    )
+            if afktype == "photo":
+                if str(reasonafk) == "None":
+                    return await message.reply_photo(
+                        photo=f"downloads/{user_id}.jpg",
+                        caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}",
+                    )
+                else:
+                    return await message.reply_photo(
+                        photo=f"downloads/{user_id}.jpg",
+                        caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}`",
+                    )
+        except Exception as e:
+            return await message.reply_text(
+                f"**{message.from_user.first_name}** is back online",
+                disable_web_page_preview=True,
+            )
+
+    
+    if len(message.command) == 1 and not message.reply_to_message:
+        details = {
+            "type": "text",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
+    elif len(message.command) > 1 and not message.reply_to_message:
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        details = {
+            "type": "text_reason",
+            "time": time.time(),
+            "data": None,
+            "reason": _reason,
+        }
+    elif (
+        len(message.command) == 1
+        and message.reply_to_message.animation
+    ):
+        _data = message.reply_to_message.animation.file_id
+        details = {
+            "type": "animation",
+            "time": time.time(),
+            "data": _data,
+            "reason": None,
+        }
+    elif (
+        len(message.command) > 1
+        and message.reply_to_message.animation
+    ):
+        _data = message.reply_to_message.animation.file_id
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        details = {
+            "type": "animation",
+            "time": time.time(),
+            "data": _data,
+            "reason": _reason,
+        }
+    elif len(message.command) == 1 and message.reply_to_message.photo:
+        await _.download_media(
+            message.reply_to_message, file_name=f"{user_id}.jpg"
+        )
+        details = {
+            "type": "photo",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
+    elif len(message.command) > 1 and message.reply_to_message.photo:
+        await _.download_media(
+            message.reply_to_message, file_name=f"{user_id}.jpg"
+        )
+        _reason = message.text.split(None, 1)[1].strip()
+        details = {
+            "type": "photo",
+            "time": time.time(),
+            "data": None,
+            "reason": _reason,
+        }
+    elif (
+        len(message.command) == 1 and message.reply_to_message.sticker
+    ):
+        if message.reply_to_message.sticker.is_animated:
+            details = {
+                "type": "text",
+                "time": time.time(),
+                "data": None,
+                "reason": None,
+            }
+        else:
+            await _.download_media(
+                message.reply_to_message, file_name=f"{user_id}.jpg"
+            )
+            details = {
+                "type": "photo",
+                "time": time.time(),
+                "data": None,
+                "reason": None,
+            }
+    elif (
+        len(message.command) > 1 and message.reply_to_message.sticker
+    ):
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        if message.reply_to_message.sticker.is_animated:
+            details = {
+                "type": "text_reason",
+                "time": time.time(),
+                "data": None,
+                "reason": _reason,
+            }
+        else:
+            await _.download_media(
+                message.reply_to_message, file_name=f"{user_id}.jpg"
+            )
+            details = {
+                "type": "photo",
+                "time": time.time(),
+                "data": None,
+                "reason": _reason,
+            }
+    else:
+        details = {
+            "type": "text",
+            "time": time.time(),
+            "data": None,
+            "reason": None,
+        }
+
+    await add_afk(user_id, details)
